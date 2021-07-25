84c6e3a215e50898242021b7333e44bcabb725b1
Tan Yanbo <tanyanbo95@hotmail.com>
2021-07-25 23:02:53 +0800

    add dummy path to add data to firestore

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const admin = require("firebase-admin");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+});
+
+const db = admin.firestore();
+
+app.get("/postacc", async (req, res) => {
+  await db.collection("users").add({
+    name: "ybfrompath",
+    email: "path@email.com",
+  });
+  res.send("Successfully added to database");
 });
 
 const PORT = process.env.PORT || 5000;
