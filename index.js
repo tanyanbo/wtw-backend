eb3284e40a308d1cfffdec4f85ea12217a363aaf
Tan Yanbo <tanyanbo95@hotmail.com>
2021-07-25 23:09:09 +0800

    another try at fixing key issue

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const admin = require("firebase-admin");
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: process.env.FIREBASE_PROJECT_ID,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
   }),
 });
