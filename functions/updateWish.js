ea4d5e6c905695aee74ac64f88ba6cce87113f88
Tan Yanbo <tanyanbo95@hotmail.com>
2021-07-26 23:29:14 +0800

    add update wish route

--- functions/updateWish.js
+++ functions/updateWish.js
@@ -1,7 +1,23 @@
 const db = require("../firebase/firestore");
 
 const updateWish = async (req, res) => {
+  try {
+    const { wishId, ...params } = req.body;
+    await db
+      .collection("wishes")
+      .doc(wishId)
+      .update({ ...params });
 
+    res.json({
+      status: "Success",
+      data: { ...req.body },
+    });
+  } catch (e) {
+    res.status(400).json({
+      status: "Fail",
+      message: "Failed to update wish",
+    });
+  }
 };
 
 module.exports = updateWish;
